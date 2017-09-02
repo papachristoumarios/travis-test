6bdc415faf49e586765dccdd1de76216f0c3dced
papachristoumarios <papachristoumarios@gmail.com>
2017-09-03 00:33:23 +0300

    fdsafs

--- test.py
+++ test.py
@@ -8,9 +8,5 @@ class SomeTest(unittest.TestCase):
         for i in range(100):
             self.assertEqual(f(i), i*i)
 
-    def test2(self):
-        for i in range(50):
-            self.assertEqual(f(i + 1), i**3)
-
 if __name__ == '__main__':
     unittest.main()
